8dfab501dc784273ef4d2f5a712aba13cbc93e2c
Kalluru-Keerti <kallurukeerti@gmail.com>
2026-02-03 14:57:31 +0530

    Update Flask response output

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
 @app.route("/")
 def hello():
-    return "Hello from OpenShift 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    text = "Hello from OpenShift 🚀<br>" * 200
+    return text
