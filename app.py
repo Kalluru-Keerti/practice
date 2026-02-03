b879b863e8b38dc0236bb1bebe8729c01b2305ce
Kalluru-Keerti <kallurukeerti@gmail.com>
2026-02-03 15:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    text = "Hello from OpenShift 🚀<br>" * 200
-    return text
+    # Create large response text
+    text = ("Hello from OpenShift 🚀 " * 200)
+
+    # Calculate response size
+    size = len(text.encode("utf-8"))
+
+    return f"""
+    <html>
+      <body>
+        <h1>Openshift Demo 🚀</h1>
+      </body>
+    </html>
+    """
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
